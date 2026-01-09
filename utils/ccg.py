cdaea57dd7ac31f0875c1b96fb95b92bcb951182
No1res <zhangyiang@zju.edu.cn>
2026-01-09 18:03:18 +0800

    Update ccg.py

--- utils/ccg.py
+++ utils/ccg.py
@@ -1,7 +1,21 @@
 import networkx as nx
 from tree_sitter import Language, Parser
 from utils.utils import CONSTANTS
+from tree_sitter import Language, Parser
+import tree_sitter_python as tspython
+import tree_sitter_java as tsjava
 
+PY_LANGUAGE = Language(tspython.language())
+JAVA_LANGUAGE = Language(tsjava.language())
+
+def get_language(lang_name: str) -> Language:
+    if lang_name.lower() == "python":
+        return PY_LANGUAGE
+    if lang_name.lower() == "java":
+        return JAVA_LANGUAGE
+    raise ValueError(f"Unsupported language: {lang_name}")
+
+language = get_language(lang_name)
 
 def python_control_dependence_graph(root_node, CCG, src_lines, parent):
     node_id = len(CCG.nodes)
@@ -688,10 +702,7 @@ def create_graph(code_lines, repo_name):
     if len(src_lines) != 0:
         src_lines[-1] = src_lines[-1].rstrip().strip('(').strip('[').strip(',')
     # Define tree-sitter parser
-    Language.build_library('./my-languages.so', ['./tree-sitter-python', './tree-sitter-java'])
-    language = Language('./my-languages.so', CONSTANTS.repos_language[repo_name])
-    parser = Parser()
-    parser.set_language(language)
+    parser = Parser(language)
 
     if len(src_lines) == 0:
         return None
